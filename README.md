91c1b91fd59b79acdda90e26da35c99183b56eb0
and-bulk <13and.bulk@gmail.com>
2021-05-17 20:27:32 +0400

    added env vars

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 ## Init project
-
 In main dir
 ```sh
 cp .env.example .env  # set env vars here
 npm ci
+```
+```sh
 npm start
+```
+```sh
+npm run dev
 ```
--- index.js
+++ index.js
@@ -1,109 +1,92 @@
-const {
-	Telegraf
-} = require('telegraf')
+const { Telegraf } = require('telegraf');
 
-const token = 'your bot token';
+require('dotenv').config();
 
-const bot = new Telegraf(token);
+const { BOT_TOKEN, FIRST_GROUP, SECOND_GROUP } = process.env;
 
-const conv1 = 111 // convID;
-const conv2 = 222 // convID;
-const convResult = 333 // convID;
+const bot = new Telegraf(BOT_TOKEN);
 
-bot.on('text', ctx => {
+bot.on('text', (ctx) => {
+  const { id } = ctx.message.chat;
 
-	// Для настройки других бесед.
-	// console.log(ctx.message);
+  if (id === FIRST_GROUP || id === SECOND_GROUP) {
+    const { text } = ctx.message;
 
-	const {
-		id
-	} = ctx.message.chat;
-
-	if (id === conv1 || id === conv2) {
-		const {
-			text
-		} = ctx.message;
-
-		if (checkExtraWords(text)) {
-			bot.telegram.sendMessage(convResult, removeExtraWords(text)).catch(e => console.error(e));
-		}
-	}
+    if (checkExtraWords(text)) {
+      bot.telegram
+        .sendMessage(convResult, removeExtraWords(text))
+        .catch((e) => console.error(e));
+    }
+  }
 });
 
-bot.on('photo', ctx => {
+bot.on('photo', (ctx) => {
+  const { id } = ctx.message.chat;
 
-	const {
-		id
-	} = ctx.message.chat
+  if (id === FIRST_GROUP || id === SECOND_GROUP) {
+    const { caption } = ctx.message;
 
-	if (id === conv1 || id === conv2) {
-
-		const {
-			caption
-		} = ctx.message;
-
-		if (checkExtraWords(caption)) {
-			bot.telegram.sendPhoto(convResult, ctx.message.photo[0].file_id, {
-				// caption: removeExtraWords(caption)
-			}).catch(e => e => console.error(e));
-		}
-	}
+    if (checkExtraWords(caption)) {
+      bot.telegram
+        .sendPhoto(convResult, ctx.message.photo[0].file_id, {
+          // caption: removeExtraWords(caption)
+        })
+        .catch((e) => (e) => console.error(e));
+    }
+  }
 });
 
-bot.on('document', ctx => {
+bot.on('document', (ctx) => {
+  const { id } = ctx.message.chat;
 
-	const {
-		id
-	} = ctx.message.chat;
+  if (id === FIRST_GROUP || id === SECOND_GROUP) {
+    const { caption } = ctx.message;
 
-	if (id === conv1 || id === conv2) {
-		const {
-			caption
-		} = ctx.message;
-
-		if (checkExtraWords(caption)) {
-			bot.telegram.sendDocument(convResult, ctx.message.document.file_id, {
-				// caption: removeExtraWords(caption)
-			}).catch(e => console.error(e));
-		}
-	}
+    if (checkExtraWords(caption)) {
+      bot.telegram
+        .sendDocument(convResult, ctx.message.document.file_id, {
+          // caption: removeExtraWords(caption)
+        })
+        .catch((e) => console.error(e));
+    }
+  }
 });
 
 bot.launch();
 
 function checkExtraWords(text) {
-	if (text) {
-		let count = 0;
-		let pos = text.indexOf(3);
+  if (text) {
+    let count = 0;
+    let pos = text.indexOf(3);
 
-		while (pos !== -1) {
-			count++;
-			pos = text.indexOf(3, pos + 1);
-		}
+    while (pos !== -1) {
+      count++;
+      pos = text.indexOf(3, pos + 1);
+    }
 
-		if (count >= 2) {
-			return true;
-		}
-	}
+    if (count >= 2) {
+      return true;
+    }
+  }
 }
 
 function removeExtraWords(text) {
-	if (text) {
-		if (text.search(/3+[\s\/\|.,;-\|из]+3+/i) !== -1) {
-			return text.replace(/3+[\s\/\|.,;-\|из]+3+/i, '');
-		} else {
-			let count = 0;
-			let result = '';
+  if (text) {
+    if (text.search(/3+[\s\/\|.,;-\|из]+3+/i) !== -1) {
+      return text.replace(/3+[\s\/\|.,;-\|из]+3+/i, '');
+    } else {
+      let count = 0;
+      let result = '';
 
-			for (let i = 0; i < text.length; i++) {
-				if (text[i] === '3' && count < 3) {
-					count++;
-				} else {
-					result += text[i];
-				}
-			}
+      for (let i = 0; i < text.length; i++) {
+        if (text[i] === '3' && count < 3) {
+          count++;
+        } else {
+          result += text[i];
+        }
+      }
 
-			return result;
-		}
-	}
+      return result;
+    }
+  }
 }
